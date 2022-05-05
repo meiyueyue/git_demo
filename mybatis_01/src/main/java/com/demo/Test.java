c5a9c3038ba025e964ba1008998ad001bb47a23d
meiyueyue <104966635+meiyueyue@users.noreply.github.com>
2022-05-05 17:46:35 +0800

    master commit02

--- mybatis_01/src/main/java/com/demo/Test.java
+++ mybatis_01/src/main/java/com/demo/Test.java
@@ -8,5 +8,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("您好01");
         System.out.println("您好02,master");
+        
+        System.out.println("您好05,master");
     }
 }
