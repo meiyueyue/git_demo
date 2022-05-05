b5af76a766d04586a364bdcf96d3c919b9994943
yingxin.li <1051006186@qq.com>
2022-05-05 16:43:43 +0800

    second commit

---
+++ mybatis_01/src/main/java/com/demo/Test.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+/**
+ * @author Yingxin.li
+ * @create 2022-05-05-16:42
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("您好01");
+    }
+}
